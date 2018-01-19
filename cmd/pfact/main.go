8d9032bd5cf74a6c71085a4136fffc2e2089c73c
Christine Dodrill <me@christine.website>
2018-01-18 20:13:21 -0800

    cmd: implement pfact

--- cmd/pfact/main.go
+++ cmd/pfact/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/Xe/ln"
+	"github.com/Xe/printerfacts/proto"
+)
+
+const defaultURL = "https://printerfacts.herokuapp.com"
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cli := proto.NewPrinterfactsProtobufClient(defaultURL, http.DefaultClient)
+	fact, err := cli.Fact(ctx, &proto.FactParams{})
+	if err != nil {
+		ln.FatalErr(ctx, err, ln.Action("fetching printer facts"))
+	}
+
+	fmt.Println(fact.Facts[0])
+}
