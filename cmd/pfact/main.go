a7cb89e186dee6becc30400d9c33dd7696de5c64
Christine Dodrill <me@christine.website>
2018-01-20 00:21:49 -0800

    cmd/pfact: add customized server address

--- cmd/pfact/main.go
+++ cmd/pfact/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"net/http"
 
@@ -11,11 +12,17 @@ import (
 
 const defaultURL = "https://printerfacts.herokuapp.com"
 
+var (
+	serverURL = flag.String("server", "https://printerfacts.herokuapp.com", "the api server the printer facts api is running on")
+)
+
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	cli := proto.NewPrinterfactsProtobufClient(defaultURL, http.DefaultClient)
+	flag.Parse()
+
+	cli := proto.NewPrinterfactsProtobufClient(*serverURL, http.DefaultClient)
 	fact, err := cli.Fact(ctx, &proto.FactParams{})
 	if err != nil {
 		ln.FatalErr(ctx, err, ln.Action("fetching printer facts"))
